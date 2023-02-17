4e52b65a6c85ca274a8b24ab2b3b735f9908b444
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2023-02-17 12:03:06 +0800

    Commmit 40

--- Calculator.py
+++ Calculator.py
@@ -55,3 +55,5 @@ button_8.grid(row=3, column=1, pady=2)
 button_9 = tk.Button(master=frame, text='9', padx=15,
                      pady=5, width=3, command=lambda: myclick(9))
 button_9.grid(row=3, column=2, pady=2)
+button_0 = tk.Button(master=frame, text='0', padx=15,
+                     pady=5, width=3, command=lambda: myclick(0))
