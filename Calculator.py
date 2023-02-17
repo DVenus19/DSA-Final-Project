f43627ad52ff22835f81526344b506b161ee6830
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2023-02-17 10:39:42 +0800

    Commmit 25

--- Calculator.py
+++ Calculator.py
@@ -36,3 +36,4 @@ button_2 = tk.Button(master=frame, text='2', padx=15,
 button_2.grid(row=1, column=1, pady=2)
 button_3 = tk.Button(master=frame, text='3', padx=15,
                      pady=5, width=3, command=lambda: myclick(3))
+button_3.grid(row=1, column=2, pady=2)
