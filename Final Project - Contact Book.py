8b87d99b526faf01e6dc766799cd801ff22f8be2
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2023-02-17 14:36:15 +0800

    Commit 1

--- Final Project - Contact Book.py
+++ Final Project - Contact Book.py
@@ -1,3 +1,5 @@
 print("^^^^^^^^^^^^^Donasco,Venus M.^^^^^^^^^^^^^")
 print("^^^^^^^^^^^^^BSCOE 2-2^^^^^^^^^^^^^")
 print("^^^^^^^^^^^^^Final Project - Calculator using Tkinter^^^^^^^^^^^^^")
+
+def merge_sort(arr):
