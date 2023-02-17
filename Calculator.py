c4f09073f913577ba5b8282046ed911558621ea4
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2023-02-17 09:45:43 +0800

    Commmit 6

--- Calculator.py
+++ Calculator.py
@@ -5,3 +5,6 @@ print("^^^^^^^^^^^^^Final Project - Calculator using Tkinter^^^^^^^^^^^^^")
 import tkinter as tk
 import tkinter.messagebox
 from tkinter.constants import SUNKEN
+
+
+window = tk.Tk()
