d7125d8553d5f5a7f82268243db8f8a6858c9d62
[DVenus19] <[donasco.venus.12232002@gmail.com]>
2023-02-17 14:40:47 +0800

    Commit 3

--- Final Project - Contact Book.py
+++ Final Project - Contact Book.py
@@ -4,3 +4,6 @@ print("^^^^^^^^^^^^^Final Project - Calculator using Tkinter^^^^^^^^^^^^^")
 
 def merge_sort(arr):
     if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
